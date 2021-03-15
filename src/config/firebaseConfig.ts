10f8a690511c0becf15612aa28ed0223bc7a5267
MuhammadMujtaba1799 <46406520+MuhammadMujtaba1799@users.noreply.github.com>
2021-03-16 02:10:18 +0500

    Update firebaseConfig.ts
    editeded

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -1,20 +1,5 @@
 var firebaseConfig = {
-    // apiKey: "AIzaSyBqec1mVHkZxWj3TfWczr835OqAKyJHsuk",
-    // authDomain: "workkarr2.firebaseapp.com",
-    // databaseURL: "https://workkarr2.firebaseio.com",
-    // projectId: "workkarr2",
-    // storageBucket: "workkarr2.appspot.com",
-    // messagingSenderId: "489894301865",
-    // appId: "1:489894301865:web:2323cd2cad326f015699b8",
-    // measurementId: "G-H4M92RCHXN"
-    apiKey: 'AIzaSyCqcOsqdzdqLe-Zc8Z-U_mTJFi9g9IcYBg',
-    authDomain: 'workkarr.firebaseapp.com',
-    databaseURL: 'https://workkarr.firebaseio.com',
-    projectId: 'workkarr',
-    storageBucket: 'workkarr.appspot.com',
-    messagingSenderId: '819428016841',
-    appId: '1:819428016841:web:269ce43e679ef4b1448318',
-    measurementId: 'G-FXHF94BVYP',
+   
   };
 
 export default firebaseConfig
